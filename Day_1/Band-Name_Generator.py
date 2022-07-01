95ffe7f9fd5a354676c24e3d64b7ca8e31f4d5c6
Mohanraj <170602mark@gmail.com>
2022-07-01 18:14:39 +0530

    Initial commit

---
+++ Day_1/Band-Name_Generator.py
@@ -0,0 +1,23 @@
+# Band Name Generator (DAY-1)
+
+print("Welcome to the Band Name Generator :)")
+
+print(" ")
+
+# we can also print string in python using single '' .
+# we can also print string using input method .
+# plus(+) for string concatenation.
+# city_name and pet_name are variables, the variables are  used to hold the User inputs.
+city_name = input("what's name of the city you grew up in?")
+
+print(" ")
+
+pet_name = input("what'is your pet name?")
+
+print(" ")
+
+print("Your band name could be : " + city_name + " " + pet_name)
+
+print(" ")
+
+print("Thank you :)")
