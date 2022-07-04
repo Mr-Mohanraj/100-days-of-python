46f3a1e46db9cf15193ae732202dd79cfce676b9
Mohanraj <170602mark@gmail.com>
2022-07-04 15:39:11 +0530

    Create rock-paper-scissors.py

---
+++ Day_4/rock-paper-scissors.py
@@ -0,0 +1,30 @@
+import random
+
+print("Welcome to Rock Paper Scissors")
+
+user_choose = int(input("What do you choose? Type 0 for Rock, 1 for Paper OR 2 for Scissors.--"))
+
+computer_choose = random.randint(0, 2)
+
+if (user_choose <= 2) and (user_choose >= 0):
+    if user_choose == computer_choose:
+        print(f"Match Draw {user_choose} and {computer_choose}")
+    elif computer_choose == 0:
+        if user_choose == 1:
+            print(f"You win! {user_choose} VS {computer_choose}")
+        else:
+            print(f"You Lose! {user_choose} VS {computer_choose}")
+    elif computer_choose == 1:
+        if user_choose == 2:
+            print(f"You win! {user_choose} VS {computer_choose}")
+        else:
+            print(f"You lose! {user_choose} VS {computer_choose}")
+    elif computer_choose == 2:
+        if user_choose == 0:
+            print(f"You win! {user_choose} VS {computer_choose}")
+    else:
+        print(f"You lose! {user_choose} VS {computer_choose}")
+else:
+    print(f"please change your number in between 0 To 2. Your input number is {user_choose}")
+    
+print("Thank you for playing")
